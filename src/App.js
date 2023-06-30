9cf19e67cc255a32d5dea0521e5e651b3b87b6f2
Madhu mohan <124799557+scully86@users.noreply.github.com>
2023-06-30 13:27:12 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,6 @@
 import React from 'react';
 import { BrowserRouter as Router,Routes,Route } from 'react-router-dom';
 import Login from './Login';
-import Main from './Main';
 
 
 function App() {
@@ -9,7 +8,6 @@ function App() {
     <Router>
     <Routes>
     <Route path='/' element={<Login />}/>
-    <Route path='main' element={<Main/>}/>
     </Routes>
     </Router>
   );
